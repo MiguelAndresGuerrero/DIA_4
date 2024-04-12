e62bb6a343d3bdb48b245a7a9086cae2bc2ea83f
Andres Guerrero <166781613+MiguelAndresGuerrero@users.noreply.github.com>
2024-04-12 12:15:48 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+
+nombre_user=str(input(print("Por favor ingresa tu nombre: \n")))
+print("Hola ", nombre_user)
+
+DineroIngresado=int(input(print("Ingresa la cantidad que deseas sacar", nombre_user )))
+print("")
